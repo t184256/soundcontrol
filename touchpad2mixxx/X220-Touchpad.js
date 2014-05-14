69bca04770d7dfd40e29a9d69152acb03649ebc3
Alexander Sosedkin <monk@unboiled.info>
2014-05-14 11:06:20 +0700

    deck change, clicktaps

--- touchpad2mixxx/X220-Touchpad.js
+++ touchpad2mixxx/X220-Touchpad.js
@@ -1,6 +1,8 @@
 function X220T() {}
 //X220T.midiChannel = 0xE0;
 //X220T.pressureChannel = 0xE1;
+//X220T.clicktapsChannel = 0xE2;
+X220T.currentDeck = 1;
 X220T.speed = 0;
 X220T.pressure = 0;
 X220T.locked = 0;
@@ -15,7 +17,8 @@ X220T.shutdown = function shutdown() {
 };
 
 X220T.scratch = function scratch(channel, control, value, status, group) {
-	var playing = engine.getValue(group, "play") ? 1. : 0.;
+	var deck = X220T.currentDeck == 1 ? "[Channel1]" : "[Channel2]";
+	var playing = engine.getValue(deck, "play") ? 1. : 0.;
 	value = value - 64
 	value = value / 10.;
 	//print("scratch " + value);
@@ -33,15 +36,38 @@ X220T.scratch = function scratch(channel, control, value, status, group) {
 		X220T.speed = 0.75 * X220T.speed + 0.25 * value;
 	if (X220T.speed > 3 || X220T.speed < -3) X220T.inertia += 1;
 	if (X220T.speed < 1 && X220T.speed > -1) X220T.inertia = 0;
-	engine.setValue(group, "scratch2", X220T.speed);
+	engine.setValue(deck, "scratch2", X220T.speed);
 };
 
 X220T.pressure = function pressure(channel, control, value, status, group) {
-	var playing = engine.getValue(group, "play") ? 1. : 0.;
+	var deck = X220T.currentDeck == 1 ? "[Channel1]" : "[Channel2]";
+	var playing = engine.getValue(deck, "play") ? 1. : 0.;
 	value = value - 64;
 	//print("pressure " + value);
 	X220T.pressure = value;
-	engine.setValue(group, "scratch2_enable", value >= 1);
+	engine.setValue(deck, "scratch2_enable", value >= 1);
 	if (value < 1) X220T.speed = playing;
 };
 
+X220T.clicktaps = function clicktaps(channel, control, value, status, group) {
+	value = value - 64;
+	var deck = X220T.currentDeck == 1 ? "[Channel1]" : "[Channel2]";
+	if (value == 0)
+		engine.setValue("[Channel1]", "keylock",
+				!engine.getValue("[Channel1]", "keylock"));
+	if (value == 1)
+		engine.setValue("[Channel2]", "keylock",
+				!engine.getValue("[Channel2]", "keylock"));
+	if (value == 2) X220T.currentDeck = 1;
+	if (value == 3) X220T.currentDeck = 2;
+	if (value == 4) {};
+	if (value == 5) {};
+	if (value == 6)
+		engine.setValue("[Channel1]", "play",
+				!engine.getValue("[Channel1]", "play"));
+	if (value == 7)
+		engine.setValue("[Channel2]", "play",
+				!engine.getValue("[Channel2]", "play"));
+	if (value == 8 || value == 9)
+		engine.setValue(deck, "play", !engine.getValue(deck, "play"));
+}
--- touchpad2mixxx/touchpad2midi.py
+++ touchpad2mixxx/touchpad2midi.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python -B
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 
-# TODO: backup, cleanup, deck change (left/right clicks)
-
 from math import sqrt, atan2, pi
 from evdev import InputDevice, ecodes
 
@@ -13,48 +11,60 @@ def pitch_bend(val, chan=0):
     val = min(max(val, -0x40), 0x40)
     return MidiOut.send_message([0xE0 + chan, 0, val+0x40])
 
-#X_CENTER = (5559 - 1320)
-#Y_CENTER = (4820 - 1361)
 X_CENTER = 3500
 Y_CENTER = 2750
+R_CORNER = 2100
 
 def main():
     PATH = '/dev/input/by-path/platform-i8042-serio-1-event-mouse'
     dev = InputDevice(PATH)
-    x, y = None, None
+    x, y, r, a = None, None, None, None
     pressure = 0
-    #angle = 0
     prev_a = None
-    difference = 0 # interstep difference
+    difference = 0 # interstep difference to be compensated later
     for event in dev.read_loop():
+        if event.type == ecodes.EV_KEY:
+            if event.code == ecodes.BTN_LEFT and event.value:
+                if not r is None:
+                    if r > R_CORNER:
+                        if -pi   < a < -pi/2: pitch_bend(4, 2)
+                        if  pi/2 < a <  pi:   pitch_bend(5, 2)
+                        if -pi/2 < a <  0:    pitch_bend(6, 2)
+                        if  0    < a <  pi/2: pitch_bend(7, 2)
+                    else:
+                        pitch_bend(8, 2)
+                else:
+                    pitch_bend(9, 2)
         if event.type == ecodes.EV_ABS:
             if event.code in (ecodes.ABS_X, ecodes.ABS_Y, ecodes.ABS_PRESSURE):
                 if event.code == ecodes.ABS_PRESSURE:
+                    if event.value > 0 and not pressure and not r is None:
+                        if r > R_CORNER:
+                            if -pi   < a < -pi/2: pitch_bend(0, 2)
+                            if  pi/2 < a <  pi:   pitch_bend(1, 2)
+                            if -pi/2 < a <  0:    pitch_bend(2, 2)
+                            if  0    < a <  pi/2: pitch_bend(3, 2)
                     pressure = event.value
                     if not pressure:
-                        prev_a = x = y = None
-                    pitch_bend(int(max(0, pressure - 32)), 1)
+                        prev_a = x = y = r = a = None
+                    if r <= R_CORNER:
+                        pitch_bend(int(max(0, pressure - 32)), 1)
                     continue
                 if event.code == ecodes.ABS_X: x = event.value - X_CENTER
                 if event.code == ecodes.ABS_Y: y = event.value - Y_CENTER
                 if (x is None or y is None): continue
-                #r = sqrt(x*x + y*y)
-                #print r
+                r = sqrt(x*x + y*y)
                 a = atan2(x, y)
-                if not prev_a is None:
+                if not prev_a is None and r <= R_CORNER:
                     da = a - prev_a
                     if da > pi: da -= pi * 2
                     if da < -pi: da += pi * 2
-                    #angle -= da / (pi * 2)
-                    #print -da / (pi * 2)
                     z = -da * 45 * 10 + difference
-                    w = int(z)
-                    w = min(max(w, -0x40), 0x40)
-                    difference = z - w
+                    w = int(z); w = min(max(w, -0x40), 0x40)
+                    difference = z - w;
                     if abs(difference) < 0.25: difference = 0
-                    #print w, difference
                     pitch_bend(w, 0)
-                #print "%f\t%f\t%f" % (r, a, angle)
+                if (r > R_CORNER): pitch_bend(0, 0)
                 prev_a = a if pressure else None
 
 if __name__ == '__main__': main()
