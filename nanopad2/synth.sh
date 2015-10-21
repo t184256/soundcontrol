c151841b84a10f4cf092c19983614f2911fba3b7
Alexander Sosedkin <monk@unboiled.info>
2015-10-22 01:43:05 +0600

    probably a lot of changes

--- nanopad2/synth.sh
+++ nanopad2/synth.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-fluidsynth ../acbass.sf2 -p fs -aalsa -f fluidsynthrc
+#fluidsynth ../acbass.sf2 -p fs -f fluidsynthrc # -aalsa
+#fluidsynth ../soundfonts/naturally_decaying_b_guitars.sf2 -p fs -f fluidsynthrc # -aalsa
+fluidsynth /usr/share/sounds/sf2/FluidR3_GM.sf2 -p fs -f fluidsynthrc # -aalsa
--- nanopad2/test.py
+++ nanopad2/test.py
@@ -6,7 +6,7 @@ DEV_NAME = 'nanoPAD2 MIDI 1'
 OUT_NAME = 'fs'
 #OUT_NAME = 'MIDI IN'
 #OUT_NAME = 'Synth input port (20116:0)'
-FIRST_NOTE = 36
+FIRST_NOTE = 24 + 4 # E, not C
 SECOND_NOTE = FIRST_NOTE + 5
 PADS1 = range(51, 35, -2)
 PADS2 = range(50, 34, -2)
@@ -32,6 +32,22 @@ print OUT_NAME, 'found at number', out_num
 midi_in = pypm.Input(dev_num)
 #midi_out = pypm.Output(pypm.GetDefaultOutputDeviceID(), 0)
 midi_out = pypm.Output(out_num, 0)
+
+def noteon(chan, note, vel):
+    midi_out.Write([[[0x90 + chan, note, vel], pypm.Time()]])
+
+def noteoff(chan, note):
+    midi_out.Write([[[0x80 + chan, note, 0], pypm.Time()]])
+
+def press(chan, base_note, vel):
+    noteon(chan, base_note, vel)
+    noteon(chan, base_note - 7, vel / 3)
+    noteon(chan, base_note + 7, vel / 4)
+    noteon(chan, base_note + 12, vel / 5)
+
+def release(chan, base_note):
+    noteoff(chan, base_note)
+    noteon(chan, base_note, 24)
 
 pressed = False
 pads1_pressed = [False] * 7
@@ -65,11 +81,11 @@ while True:
         # pad pressed
         if b == PADS1[-1]:
             # noteon for the first pad row
-            midi_out.Write([[[0x90+0, FIRST_NOTE + 0, c], pypm.Time()]])
+            press(0, FIRST_NOTE, c)
             continue
         if b == PADS2[-1]:
             # noteon for the second pad row
-            midi_out.Write([[[0x90+1, SECOND_NOTE + 0, c], pypm.Time()]])
+            press(1, SECOND_NOTE, c)
             continue
         if b in PADS1: pads1_pressed[PADS1.index(b)] = True
         if b in PADS2: pads2_pressed[PADS2.index(b)] = True
@@ -77,12 +93,12 @@ while True:
         if b == PADS1[-1]:
             # noteoff for the first pad row
             if not pressed:
-                midi_out.Write([[[0x80+0, FIRST_NOTE + 0, c], pypm.Time()]])
+                release(0, FIRST_NOTE)
             continue
         if b == PADS2[-1]:
             # noteoff for the second pad row
             if not pressed:
-                midi_out.Write([[[0x80+1, SECOND_NOTE + 0, c], pypm.Time()+1000]])
+                release(1, SECOND_NOTE)
             continue
         if b in PADS1: pads1_pressed[PADS1.index(b)] = False
         if b in PADS2: pads2_pressed[PADS2.index(b)] = False
